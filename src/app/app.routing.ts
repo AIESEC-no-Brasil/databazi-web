ddfba8fc9c2a6f1354f45b602909777629f9e418
Luiz Reis <luiz@devmagic.com.br>
2018-08-29 16:47:05 -0300

    feat(index / style): first commit with style.css

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,12 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageGvComponent } from './landing-page-gv/landing-page-gv.component';
 
 export const router : Routes = [
     { path: '', redirectTo:'landing-page', pathMatch:'full'},
-    { path: 'landing-page', component: LandingPageComponent }
+    { path: 'landing-page', component: LandingPageComponent },
+    { path: 'landing-page-gv', component: LandingPageGvComponent }
 ]
 
 export const routes : ModuleWithProviders = RouterModule.forRoot(router, { useHash : false });
