59e21a6be6348dc112c8177b949fff2b86f34f90
Luiz Reis <luiz@devmagic.com.br>
2018-08-31 17:29:20 -0300

    feat(landing-page-gt): landing page gt was done, with responsive layout, images and icons

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LandingPageGvComponent } from './landing-page-gv/landing-page-gv.component';
+import { LandingPageGtComponent } from './landing-page-gt/landing-page-gt.component';
 
 export const router : Routes = [
     { path: '', redirectTo:'landing-page', pathMatch:'full'},
     { path: 'landing-page', component: LandingPageComponent },
-    { path: 'landing-page-gv', component: LandingPageGvComponent }
+    { path: 'landing-page-gv', component: LandingPageGvComponent },
+    { path: 'landing-page-gt', component: LandingPageGtComponent }
 ]
 
 export const routes : ModuleWithProviders = RouterModule.forRoot(router, { useHash : false });
