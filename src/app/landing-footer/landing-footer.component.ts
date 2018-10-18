de9612fad1335022c327b8972d84ad51877793f6
Luiz Reis <luiz@devmagic.com.br>
2018-10-18 14:41:02 -0300

    feat(landing page / footer): responsive layout fixed, new design 'footer' area, carousel now stops when 'mouse over' event

--- src/app/landing-footer/landing-footer.component.ts
+++ src/app/landing-footer/landing-footer.component.ts
@@ -11,6 +11,7 @@ export class LandingFooterComponent implements OnInit {
 
   partners: any;
   sliderPosition: number = 0;
+  timer:any;
 
   navTo(partnerPage) {
     window.open(partnerPage, '_blank');
@@ -19,78 +20,78 @@ export class LandingFooterComponent implements OnInit {
   constructor(private router: Router) {
     this.partners = [
       {
-        title: 'assistcard',
+        title: 'Assist Card',
         img: '../../assets/images/partners/assistcard.png',
         site: 'https://www.assistcard.com/b2c/aiesec-br',
       },
       {
-        title: 'delivery_much',
+        title: 'Delivery Much',
         img: '../../assets/images/partners/delivery_much.png',
         site: 'https://www.deliverymuch.com.br/',
       },
       {
-        title: 'dhl',
+        title: 'DHL',
         img: '../../assets/images/partners/dhl.png',
         site: 'https://aiesec.org/partner/286836',
       },
       {
-        title: 'electrolux',
+        title: 'Electrolux',
         img: '../../assets/images/partners/electrolux.png',
         site: 'https://aiesec.org/partner/272692',
       },
       {
-        title: 'hyva',
+        title: 'Hyva',
         img: '../../assets/images/partners/hyva.png',
         site: 'https://aiesec.org/partner/344322',
 
       },
       {
-        title: 'leroymerlin',
+        title: 'Leroy Merlin',
         img: '../../assets/images/partners/leroymerlin.png',
         site: 'https://www.leroymerlin.com.br/blog',
       },
       {
-        title: 'mckinseyecompany',
+        title: 'McKinsey & Company',
         img: '../../assets/images/partners/mckinseyecompany.png',
         site: 'https://www.mckinsey.com.br/',
 
       },
       {
-        title: 'mondelez',
+        title: 'Mondelez',
         img: '../../assets/images/partners/mondelez.png',
         site: 'https://br.mondelezinternational.com/home',
 
       },
       {
-        title: 'nestle',
+        title: 'Nestle',
         img: '../../assets/images/partners/nestle.png',
         site: 'https://www.nestle.com.br/',
       },
       {
-        title: 'nokia',
+        title: 'Nokia',
         img: '../../assets/images/partners/nokia.png',
         site: 'https://aiesec.org/partner/330494',
 
       },
       {
-        title: 'resultadosdigitais',
+        title: 'Resultados Digitais',
         img: '../../assets/images/partners/resultadosdigitais.png',
         site: 'https://resultadosdigitais.com.br/',
 
       },
       {
-        title: 'sap',
+        title: 'SAP',
         img: '../../assets/images/partners/sap.png',
         site: 'https://www.sap.com/brazil/about.html',
 
       },
       {
-        title: 'votorantimcimentos',
+        title: 'Votorantim Cimentos',
         img: '../../assets/images/partners/votorantimcimentos.png',
         site: 'https://www.votorantimcimentos.com.br/',
       },
       {
-        title: 'Whirlpoow',
+        title: 'Whirlpool',
         img: '../../assets/images/partners/Whirlpoow.png',
         site: 'https://www.whirlpool.com.br/',
       },
@@ -129,9 +130,13 @@ export class LandingFooterComponent implements OnInit {
   }
 
   sliderAnimation() {
-    setInterval(() => {
+    this.timer = setInterval(() => {
       this.moveRight()
     }, 10000);
+  }
+
+  stopAnimation(){
+    clearInterval(this.timer)
   }
 
   openUrl(site){
