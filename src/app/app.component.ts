a83188006f925f74dee8cc489fb6071ccb80c40f
Luiz Reis <luiz@devmagic.com.br>
2018-11-01 11:35:03 -0300

    fix(index/appcomponent): remove track 'embedded' form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,9 +19,9 @@ export class AppComponent {
   changeOfRoutes(){
     this.activate.queryParams.subscribe((param: any) => {
       if (!param['embedded']){
-        this.window.fbq('track', 'PageView');
         this.window.fbq('init', '531154527045235');
         this.window.fbq('init', '2083757008560964');
+        this.window.fbq('track', 'PageView');
       }
     });
   }
