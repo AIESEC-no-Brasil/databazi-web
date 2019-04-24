84e0f1167239d2b799795a785fbafc07afda8d9f
Gabriel <gabrielreis@devmagic.com.br>
2019-04-24 16:38:52 -0300

    feat(landing-page): added cookies policy snackbar to notify user
    AIESEC-273 - https://devmagic.atlassian.net/browse/AIESEC-273

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -40,6 +40,17 @@ export class LandingPageComponent implements OnInit {
         break;
     }
   }
+
+  showSnackBar() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    //setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  };
+
+  hideSnackBar() {
+    var x = document.getElementById("snackbar");
+    x.className = "";
+  };
   
 	moveLeft() {
     if ((this.sliderPosition - 100) < 0){
@@ -65,7 +76,8 @@ export class LandingPageComponent implements OnInit {
 
   ngOnInit() {
     this.sliderAnimation();
-    window.scrollTo(0,0)
+    window.scrollTo(0,0);
+    this.showSnackBar();
   }
 
   sliderAnimation(){
