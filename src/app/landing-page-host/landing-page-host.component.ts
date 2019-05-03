c28ea1f9b79f16e6ab7aa08c6cc1c4d5cce235f2
Claudio Oliveira <claudio.oliveira@outlook.com.br>
2019-05-03 13:09:46 -0300

    fix(landing-host): layout updates

--- src/app/landing-page-host/landing-page-host.component.ts
+++ src/app/landing-page-host/landing-page-host.component.ts
@@ -9,18 +9,23 @@ export class LandingPageHostComponent implements OnInit {
 
   hostRequirements = [
     {
+      icon: 'fas fa-bed',
       description: 'Ao menos um colchão para o intercambista. Não é necessário quarto individual.'
     },
     {
+      icon: 'fas fa-key',
       description: 'Acesso ao banheiro, áreas comuns da casa e a residência.'
     },
     {
+      icon: 'fas fa-wifi',
       description: 'Acesso à internet.'
     },
     {
+      icon: 'fas fa-tint',
       description: 'Água para uso.'
     },
     {
+      icon: 'fas fa-globe-americas',
       description: 'Vontade de conhecer outras culturas.'
     }
   ]
