12c4534e6d6433cdbdc486e830e640686349900d
Luiz Reis <luiz@devmagic.com.br>
2019-05-23 12:58:02 -0300

    chore(landing page ge): links updated

--- src/app/landing-page-ge/landing-page-ge.component.ts
+++ src/app/landing-page-ge/landing-page-ge.component.ts
@@ -15,7 +15,7 @@ export class LandingPageGeComponent implements OnInit {
       image: './assets/products/ge/country-egito.jpg',
       flag: './assets/countries/egypt.svg',
       description: ' Minha experiência no Egito foi um divisor de águas tanto como pessoa quanto como profissional. Ao fazer o programa empreendedor global, pude viver uma troca muito intensa com pessoas de diversas nacionalidades.',
-      link: 'https://aiesec.org/search?home_mcs=1609&type=5&earliest_start_date=2019-06-01&sort=-duration_min',
+      link: 'https://aiesec.org/search?home_mcs=1609&type=5&earliest_start_date=2019-06-16&sort=-duration_min',
       active: true
     },
     { 
@@ -23,7 +23,7 @@ export class LandingPageGeComponent implements OnInit {
       image: './assets/products/ge/country-hungary.png',
       flag: './assets/countries/ukraine.svg',
       description: "Realizei o programa Empreendedor Global pela AIESEC e fui parar na Ucrânia! Não sabia o que esperar daquele país do leste europeu, que, já adianto, me impressionou tanto! Trabalhei em uma StartUp chamada Twiga, onde encontrei pessoas que levarei para toda a vida, que me ensinaram muito sobre marketing e sobre a cultura ucraniana.", 
-      link: 'https://aiesec.org/search?home_mcs=1610&type=5&earliest_start_date=2019-05-31&sort=-duration_min',
+      link: 'https://aiesec.org/search?home_mcs=1610&type=5&earliest_start_date=2019-06-16&sort=-duration_min',
       active: false
     },
     { 
@@ -31,7 +31,7 @@ export class LandingPageGeComponent implements OnInit {
       image: './assets/products/ge/country-argentina.jpg',
       flag: './assets/countries/argentina.svg',
       description: 'Na Argentina trabalhei em uma Startup na área de marketing em que pude ter todo o suporte e liberdade! Além disso, meus chefes eram incríveis e me ajudaram e me ensinaram muito. Todo o dia era uma luta nova, erros e acertos e isso me fez crescer como pessoa', 
-      link: 'https://aiesec.org/search?home_mcs=1535&type=5&earliest_start_date=2019-06-01&sort=-duration_min',
+      link: 'https://aiesec.org/search?home_mcs=1535&type=5&earliest_start_date=2019-06-16&sort=-duration_min',
       active: false
     },
     { 
@@ -39,7 +39,7 @@ export class LandingPageGeComponent implements OnInit {
       image: './assets/products/ge/country-mexico.jpg',
       flag: './assets/countries/mexico.svg',
       description: ' Tive a oportunidade de através do Empreendedor Global, me apaixonar pelo México, me desenvolver profissionalmente, que me deu a visão de como é começar um negócio e empreender e também me colocou em contato com atividades de diferentes áreas, me dando uma visão ampla das atividades de uma startup.', 
-      link: 'https://aiesec.org/search?home_mcs=1589&type=5&earliest_start_date=2019-06-01&sort=-duration_min',
+      link: 'https://aiesec.org/search?home_mcs=1589&type=5&earliest_start_date=2019-06-16&sort=-duration_min',
       active: false
     },
     { 
@@ -47,7 +47,7 @@ export class LandingPageGeComponent implements OnInit {
       image: './assets/products/ge/country-peru.jpeg',
       flag: './assets/countries/peru.svg',
       description: 'Minha experiência de viver no Peru foi muito enriquecedora, principalmente no âmbito profissional, pois como estrangeira em outro país, as respostas para as minhas dúvidas estavam sempre em escutar, agir e me esforçar para entender como funciona tudo e me adaptar a uma equipe em uma Startup de um setor tão diferente do meu.', 
-      link: 'https://aiesec.org/search?home_mcs=1553&type=5&earliest_start_date=2019-06-01&sort=-duration_min',
+      link: 'https://aiesec.org/search?home_mcs=1553&type=5&earliest_start_date=2019-06-16&sort=-duration_min',
       active: false
     }
   ]
