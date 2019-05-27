3620629269c36c86eac6d74f2abd7756524735cd
Luiz Reis <luiz@devmagic.com.br>
2019-05-27 17:00:40 -0300

    fix(forms *): email (message and pattern) fixed

--- src/app/form-ge/form-ge.component.ts
+++ src/app/form-ge/form-ge.component.ts
@@ -120,7 +120,8 @@ export class FormGeComponent implements OnInit {
         Validators.required
       ]),
       email: new FormControl(this.user.email, [
-        Validators.required
+        Validators.required,
+        Validators.pattern('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
       ]),
       birthdate: new FormControl(this.user.birthdate, [
         Validators.required
@@ -384,7 +385,7 @@ export class FormGeComponent implements OnInit {
       ge_participant: {
         fullname: this.user.fullname,
         cellphone: this.user.cellphone.replace(/[()_-]/g, ''),
-        email: this.user.email,
+        email: this.user.email.toLowerCase(),
         password: this.user.password,
         birthdate: moment(this.user.birthdate, 'DDMMYYYY').format('DD/MM/YYYY'),
         local_committee_id: +this.user.local_committee.id,
