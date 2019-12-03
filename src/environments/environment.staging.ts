50662bf2135fa9c8c158599ccb7d526e3409cafa
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2019-12-03 14:18:29 -0300

    fix(environment): removes localhost from staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://qa-databazi.aiesec.org.br',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://qa-databazi.aiesec.org.br',
   GAid: 'UA-29562174-6',
   GTMid: 'GTM-567W86C',
   HJid: '1469982',
